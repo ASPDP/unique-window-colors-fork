3cd34613059bf94773d4108b5d0e1fc223d7b136
Raphael Vaz <raphael@wrath.codes>
2025-03-26 15:25:33 -0300

    feat: fixed extensions dependencies and failing linting job

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import { defineExtension, useDisposable } from 'reactive-vscode'
-import { extensions, window } from 'vscode'
 import { registerCommands } from './commands'
 import { setupNeovimModeManager } from './neovimModeManager'
 import { setupThemeManager } from './themeManager'
@@ -7,7 +6,6 @@ import { logger } from './utils'
 
 const { activate, deactivate } = defineExtension(async () => {
   logger.info('nvim-ui-plus extension activated')
-
 
   // Set up the mode manager
   const modeManager = setupNeovimModeManager()
